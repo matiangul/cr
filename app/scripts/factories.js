2241e615f67e1ef2362c8681e6d6ac469a425c8b
matiangul <mateusz.angulski@gmail.com>
2014-01-06 16:34:34 +0100

    working mario map with new players

--- app/scripts/factories.js
+++ app/scripts/factories.js
@@ -1,43 +1,121 @@
+CR.factories.Exception = stampit().state({
+    name: "Exception",
+    message: "Sth bad happened"
+});
 CR.factories.BaseClass = stampit().enclose(function() {
-        var _id = this.id;
-        delete this.id;
+    var _id = this.id;
+    delete this.id;
 
-        this.getId = function() {
-            return _id;
+    this.getId = function() {
+        return _id;
+    }
+}).methods({
+    lazyLoad: function(property, factory) {
+        if (this[property] === null) {
+            this[property] = factory();
         }
-    }).methods({
-        lazyLoad: function(property, factory) {
-            if (this[property] === null) {
-                this[property] = factory();
-            }
-            return this[property];
+        return this[property];
+    },
+    throwIfNull: function(property) {
+        if (this[property] === null || typeof this[property] === typeof undefined) {
+            throw CR.factories.Exception({
+                name: "RuntimeException",
+                message: "Property not set"
+            });
         }
-    });
+    },
+    returnIfSet: function(obj) {
+        if (typeof obj !== typeof undefined) {
+            return obj;
+        } else {
+            throw CR.factories.Exception({
+                name: "InvalidObject",
+                message: "Object with this name is not set"
+            });
+        }
+    }
+});
 CR.factories.ActionSprite = stampit.compose(CR.factories.BaseClass, 
-        stampit().state({
-            name: "",
-            image: "",
-            frameWidth: 0,
-            frameHeight: 0,
-            frameCount: 0,
-            action: null
-        })
-    );
+    stampit().state({
+        name: "",
+        image: "",
+        frameWidth: 0,
+        frameHeight: 0,
+        frameCount: 0,
+        action: null,
+
+    })
+);
 CR.factories.Action = stampit.compose(CR.factories.BaseClass, 
-        stampit().state({
-            speed: 0,
-            frame: 0,
-            sprite: null,
-            actor: null
-        })
-    );
+    stampit().state({
+        speed: 0,
+        frame: 0,
+        sprite: null,
+        actor: null
+    }).methods({
+        getActor: function () {
+            this.throwIfNull('actor');
+        }
+    })
+);
 CR.factories.Actor = stampit.compose(CR.factories.BaseClass, 
-        stampit().state({
-            bombs: 0,
-            granades: 0,
-            speedups: 0,
-            action: null,
-            position: null,
-            actions: {}
-        })
-    );
+    stampit().state({
+        name: "",
+        bombs: 0,
+        granades: 0,
+        speedups: 0,
+        action: null,
+        position: null
+    }).methods({
+        actions: null,
+        getPosition: function() {
+            this.throwIfNull('position');
+            return this.position;
+        },
+        getAction: function() {
+            this.throwIfNull('action');
+            return this.action;
+        }
+    })
+);
+CR.obj.actionSpriteActorMatcher = stampit().compose(CR.factories.BaseClass,
+    stampit().enclose({
+        this.match = function (actorName, actionClass) {
+            var table = {
+                'mummy': {
+                    'Run': []
+                }
+            };
+
+            return this.returnIfSet(table[actorName][actionClass]));
+        }
+    })
+)(); //singleton
+
+CR.factories.Mummy = stampit.compose(CR.factories.Actor,
+    stampit().state({
+        name: "mummy"
+    }).enclose({
+        var avActions = [
+            ['run', 'Run']
+        ];
+        if (this.actions === null) {
+            this.actions = {};
+            for (var i = 0; i < avActions.length; i++) {
+                this.actions[avActions[i][0]] = CR.factories[avActions[i][1]]({actor: this});
+            }
+        }
+    })
+);
+
+CR.factories.Run = stampit.compose(CR.factories.Action,
+    stampit().enclose({
+        switch(this.getActor().name) {
+            case "mummy": 
+                this.sprite = CR.factories.ActionSprite();
+                break;
+            default:
+                break;
+        }
+    })
+);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,47 +1,214 @@
-CR.game = new Phaser.Game(655, 208, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+Phaser.Tilemap.prototype.setCollisionByIndexAndFace = function (index, layer, recalculate, top, bottom, left, right) {
+
+    if (typeof layer === 'undefined')
+    {
+        layer = this.currentLayer;
+    }
+    else if (typeof layer === 'string')
+    {
+        layer = this.getLayerIndex(layer);
+    }
+    else if (layer instanceof Phaser.TilemapLayer)
+    {
+        layer = layer.index;
+    }
+
+    if (typeof recalculate === "undefined") { recalculate = true; }
+
+    for (var y = 0; y < this.layers[layer].height ; y++)
+    {
+        for (var x = 0; x < this.layers[layer].width; x++)
+        {
+            var tile = this.layers[layer].data[y][x];
+
+            if (tile && tile.index === index)
+            {
+                tile.collides = true;
+                tile.faceTop = top;
+                tile.faceBottom = bottom;
+                tile.faceLeft = left;
+                tile.faceRight = right;
+            }
+        }
+    }
+
+    if (recalculate)
+    {
+        this.calculateFaces(layer);
+    }
+
+    return layer;
+
+};
+
+Phaser.Tilemap.prototype.setCollisionRange = function (start, stop, top, bottom, left, right, layer) {
+
+    if (start > stop)
+    {
+        return;
+    }
+
+    for (var i = start; i <= stop; i++)
+    {
+        this.setCollisionByIndexAndFace(i, layer, true, top, bottom, left, right);
+    }
+
+};
+
+Phaser.Utils.Debug.prototype.renderSpriteBody = function (sprite, color) {
+
+    if (this.context == null || sprite.body.touching.none === true)
+    {
+        return;
+    }
+
+    color = color || 'rgba(255,0,255, 0.3)';
+
+    this.start(0, 0, color);
+
+    this.context.fillStyle = color;
+    this.context.fillRect(sprite.body.screenX, sprite.body.screenY, sprite.body.width, sprite.body.height);
+
+    this.stop();
+    this.start(0, 0, color);
+
+    this.context.beginPath();
+    this.context.strokeStyle = '#000000';
+
+    if (sprite.body.touching.up)
+    {
+        this.context.moveTo(sprite.body.screenX, sprite.body.screenY);
+        this.context.lineTo(sprite.body.screenX + sprite.body.width, sprite.body.screenY);
+    }
+
+    if (sprite.body.touching.down)
+    {
+        this.context.moveTo(sprite.body.screenX, sprite.body.screenY + sprite.body.height);
+        this.context.lineTo(sprite.body.screenX + sprite.body.width, sprite.body.screenY + sprite.body.height);
+    }
+
+    if (sprite.body.touching.left)
+    {
+        this.context.moveTo(sprite.body.screenX, sprite.body.screenY);
+        this.context.lineTo(sprite.body.screenX, sprite.body.screenY + sprite.body.height);
+    }
+
+    if (sprite.body.touching.right)
+    {
+        this.context.moveTo(sprite.body.screenX + sprite.body.width, sprite.body.screenY);
+        this.context.lineTo(sprite.body.screenX + sprite.body.width, sprite.body.screenY + sprite.body.height);
+    }
+
+    this.context.stroke();
+
+    this.stop();
+
+};
+
+Phaser.Utils.Debug.prototype.renderSpriteBounds = function (sprite, color, fill) {
+
+    if (this.context == null)
+    {
+        return;
+    }
+
+    color = color || 'rgb(255,0,255)';
+    colorBody = 'rgb(0,0,0)';
+
+    if (typeof fill === 'undefined') { fill = false; }
+
+    this.start(0, 0, color);
+
+    if (fill)
+    {
+        this.context.fillStyle = color;
+        this.context.fillRect(sprite.bounds.x, sprite.bounds.y, sprite.bounds.width, sprite.bounds.height);
+    }
+    else
+    {
+        this.context.strokeStyle = color;
+        this.context.strokeRect(sprite.bounds.x, sprite.bounds.y, sprite.bounds.width, sprite.bounds.height);
+        this.context.stroke();
+        this.context.strokeStyle = colorBody;
+        this.context.strokeRect(sprite.body.screenX, sprite.body.screenY, sprite.body.width, sprite.body.height);
+        this.context.stroke();
+    }
+
+    this.stop();
+
+};
+
+var CR = {};
+
+CR.game = new Phaser.Game(655, 208, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-    CR.game.load.tilemap('mario', 'assets/maps/mario1.png', 'assets/maps/mario1.json', null, Phaser.Tilemap.JSON);
-    CR.game.load.spritesheet('mummy', 'assets/sprites/metalslug_mummy37x45.png', 37, 45, 18);
+    CR.game.load.tilemap('map', 'assets/maps/map.json', null, Phaser.Tilemap.TILED_JSON);
+    CR.game.load.image('tiles', 'assets/maps/map.png');
+    CR.game.load.spritesheet('creature', 'assets/sprites/creature.png', 37, 45, 18);
+    CR.game.load.spritesheet('player', 'assets/sprites/player.png', 58, 96, 5);
 }
 
 function create() {
+    /*map settings*/
+    CR.map = CR.game.add.tilemap('map');
+    CR.map.addTilesetImage('map.png', 'tiles');
+    CR.layer = CR.map.createLayer(0);
+    //CR.layer.fixedToCamera = false;
+    //CR.layer.debug = true;
+    CR.layer.resizeWorld();
+    /*floor*/
+    CR.map.setCollisionRange(80, 97, true, true, true, true);
+    /*one-ways*/
+    CR.map.setCollisionRange(15, 17, true, true, false, true);
+    // CR.map.setCollisionByExclusion([7, 32, 35, 36, 47]);
+
+    /*creature*/
+    CR.creature = CR.game.add.sprite(100, 50, 'creature');
+    CR.creature.body.collideWorldBounds = false;
+    CR.creature.body.setSize(22, 45, 6, 0);
+    // CR.creature.anchor.setTo(0.5, 0.5);
+    //CR.creature.bounce.y = 0.4;
+    CR.creature.animations.add('run');
+    CR.creature.animations.play('run', 30, true);
+
+    /*player*/
+    CR.player = CR.game.add.sprite(240, 50, 'player');
+    CR.player.body.collideWorldBounds = false;
+    CR.player.body.setSize(38, 96, 6, 0);
+    // CR.player.anchor.setTo(0.5, 0.5);
+    CR.player.scale.setTo(0.6379, 0.46875);
+    //CR.player.bounce.y = 0.4;
+    CR.player.animations.add('run');
+    CR.player.animations.play('run', 10, true);
+
+    /*game settings*/
     CR.game.stage.backgroundColor = '#787878';
-    CR.obj.map = CR.game.add.tilemap(0, 0, 'mario');
-    CR.obj.map.setCollisionRange(80, 97, true, true, true, true);
-    CR.obj.map.setCollisionRange(15, 17, true, true, false, true);
-    mummy = CR.game.add.sprite(100, 55, 'mummy');
-    bot = CR.game.add.sprite(250, 50, 'mummy');
-    bot.scale = new Phaser.Point(0.7, 0.7);
-    bot.animations.add('run');
-    mummy.animations.add('walk');
-    bot.animations.play('run', 30, true);
-    mummy.animations.play('walk', 50, true);
-
-    mummy.body.bounce.y = 0.4;
-    mummy.body.collideWorldBounds = true;
-
-    bot.body.bounce.y = 0.4;
-    bot.body.collideWorldBounds = true;
-
-    CR.game.camera.follow(bot);
-
+    CR.game.camera.follow(CR.player);
+    // CR.game.camera.deadzone = new Phaser.Rectangle(160, 160, layer.renderWidth-320, layer.renderHeight-320);
 }
 
 function update() {
-    CR.obj.map.collide(mummy);
-    CR.obj.map.collide(bot);
+    CR.game.physics.collide(CR.player, CR.layer);
+    CR.game.physics.collide(CR.creature, CR.layer);
+    CR.game.physics.collide(CR.player, CR.creature);
+    /*creature*/
+    CR.creature.body.acceleration.y = 300;
+    /*player*/
+    CR.player.body.acceleration.y = 300;
+    runForest(CR.creature, CR.player);
+}
 
-    //mummy.body.velocity.x = 0;
-    mummy.body.acceleration.y = 500;
+function render() {
+    // CR.game.debug.renderText("DEBUG", 100, 100);
+    // CR.game.debug.renderSpriteBody(CR.player);
+    CR.game.debug.renderSpriteBounds(CR.player);
+    // CR.game.debug.renderSpriteBody(CR.creature);
+    CR.game.debug.renderSpriteBounds(CR.creature);
+}
 
-    //bot.body.velocity.x = 0;
-    bot.body.acceleration.y = 500;
-
-    bot.body.velocity.x += 0.05;
-    mummy.body.velocity.x += 0.1;
-    if (mummy.body.x >= 220) {
-        bot.body.velocity.x = 0;
-        mummy.body.velocity.x = 0;
-    }
+function runForest(spy, forest)
+{
+    spy.body.velocity.x = 35;
+    forest.body.velocity.x = 25;
 }
