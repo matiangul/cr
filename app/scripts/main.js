019b8a4de53fa5be4efc016b17e12e8894e27f22
matiangul <mateusz.angulski@gmail.com>
2014-01-06 18:40:58 +0100

    nowa mapa i dodatkowe elementy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -138,13 +138,20 @@ Phaser.Utils.Debug.prototype.renderSpriteBounds = function (sprite, color, fill)
 
 };
 
-var CR = {};
+var CR = {},
+    debug = false;
 
-CR.game = new Phaser.Game(655, 208, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render });
+CR.game = new Phaser.Game(655, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-    CR.game.load.tilemap('map', 'assets/maps/map.json', null, Phaser.Tilemap.TILED_JSON);
-    CR.game.load.image('tiles', 'assets/maps/map.png');
+    CR.game.load.tilemap('map', 'assets/maps/creature.json', null, Phaser.Tilemap.TILED_JSON);
+    CR.game.load.image('tiles', 'assets/maps/creatureMap.png');
+    CR.game.load.image('bomb', 'assets/images/bomb.png');
+    CR.game.load.image('cage', 'assets/images/cage.png');
+    CR.game.load.image('granade', 'assets/images/granade.png');
+    CR.game.load.image('line', 'assets/images/line.png');
+    CR.game.load.image('smoke', 'assets/images/smoke.png');
+    CR.game.load.image('thunder', 'assets/images/thunder.png');
     CR.game.load.spritesheet('creature', 'assets/sprites/creature.png', 37, 45, 18);
     CR.game.load.spritesheet('player', 'assets/sprites/player.png', 58, 96, 5);
 }
@@ -152,33 +159,37 @@ function preload() {
 function create() {
     /*map settings*/
     CR.map = CR.game.add.tilemap('map');
-    CR.map.addTilesetImage('map.png', 'tiles');
+    CR.map.addTilesetImage('creature', 'tiles');
     CR.layer = CR.map.createLayer(0);
-    //CR.layer.fixedToCamera = false;
-    //CR.layer.debug = true;
+    // CR.layer.fixedToCamera = true;
+    // CR.layer.debug = true;
     CR.layer.resizeWorld();
-    /*floor*/
-    CR.map.setCollisionRange(80, 97, true, true, true, true);
-    /*one-ways*/
-    CR.map.setCollisionRange(15, 17, true, true, false, true);
-    // CR.map.setCollisionByExclusion([7, 32, 35, 36, 47]);
+    CR.map.setCollisionByIndex(6);
+    CR.map.setCollisionByIndex(8);
+
+    CR.game.add.sprite(100, 250, 'bomb');
+    CR.game.add.sprite(120, 250, 'cage');
+    CR.game.add.sprite(140, 250, 'granade');
+    CR.game.add.sprite(160, 250, 'line');
+    CR.game.add.sprite(180, 250, 'smoke');
+    CR.game.add.sprite(200, 250, 'thunder');
 
     /*creature*/
     CR.creature = CR.game.add.sprite(100, 50, 'creature');
-    CR.creature.body.collideWorldBounds = false;
+    CR.creature.body.collideWorldBounds = true;
     CR.creature.body.setSize(22, 45, 6, 0);
     // CR.creature.anchor.setTo(0.5, 0.5);
-    //CR.creature.bounce.y = 0.4;
+    // CR.creature.bounce.y = 0.4;
     CR.creature.animations.add('run');
     CR.creature.animations.play('run', 30, true);
 
     /*player*/
     CR.player = CR.game.add.sprite(240, 50, 'player');
-    CR.player.body.collideWorldBounds = false;
+    CR.player.body.collideWorldBounds = true;
     CR.player.body.setSize(38, 96, 6, 0);
-    // CR.player.anchor.setTo(0.5, 0.5);
     CR.player.scale.setTo(0.6379, 0.46875);
-    //CR.player.bounce.y = 0.4;
+    // CR.player.anchor.setTo(0.5, 0.5);
+    // CR.player.bounce.y = 0.4;
     CR.player.animations.add('run');
     CR.player.animations.play('run', 10, true);
 
@@ -196,15 +207,18 @@ function update() {
     CR.creature.body.acceleration.y = 300;
     /*player*/
     CR.player.body.acceleration.y = 300;
+
     runForest(CR.creature, CR.player);
 }
 
 function render() {
-    // CR.game.debug.renderText("DEBUG", 100, 100);
-    // CR.game.debug.renderSpriteBody(CR.player);
-    CR.game.debug.renderSpriteBounds(CR.player);
-    // CR.game.debug.renderSpriteBody(CR.creature);
-    CR.game.debug.renderSpriteBounds(CR.creature);
+    if (debug) {
+        // CR.game.debug.renderText("DEBUG", 100, 100);
+        CR.game.debug.renderSpriteBody(CR.player);
+        CR.game.debug.renderSpriteBounds(CR.player);
+        CR.game.debug.renderSpriteBody(CR.creature);
+        CR.game.debug.renderSpriteBounds(CR.creature);
+    }
 }
 
 function runForest(spy, forest)
